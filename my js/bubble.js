66579745679ca8c77990af34ddcc421e0615c427
imrohit150 <imrohit.150@gmail.com>
2022-01-16 13:15:08 +0530

    My Portfolio

---
+++ my js/bubble.js
@@ -0,0 +1,56 @@
+// -------------bubbleOne-------------------------------------------------
+var scene = document.getElementById('sceneOne');
+var parallaxInstance = new Parallax(scene);
+
+var element = document.getElementById('sceneOne');
+element.style.removeProperty("transform");
+element.style.removeProperty("position");
+element.style.removeProperty("transform-style");
+
+var element = document.getElementById('bubblejsOne');
+element.style.removeProperty("position");
+element.style.removeProperty("left");
+element.style.removeProperty("top");
+
+
+// -------------bubbleTwo-------------------------------------------------
+var scene = document.getElementById('sceneTwo');
+var parallaxInstance = new Parallax(scene);
+
+var element = document.getElementById('sceneTwo');
+element.style.removeProperty("transform");
+element.style.removeProperty("position");
+element.style.removeProperty("transform-style");
+
+var element = document.getElementById('bubblejsTwo');
+element.style.removeProperty("position");
+element.style.removeProperty("left");
+element.style.removeProperty("top");
+
+// -------------bubbleThree-------------------------------------------------
+var scene = document.getElementById('sceneThree');
+var parallaxInstance = new Parallax(scene);
+
+var element = document.getElementById('sceneThree');
+element.style.removeProperty("transform");
+element.style.removeProperty("position");
+element.style.removeProperty("transform-style");
+
+var element = document.getElementById('bubblejsThree');
+element.style.removeProperty("position");
+element.style.removeProperty("left");
+element.style.removeProperty("top");
+
+// -------------bubbleFour-------------------------------------------------
+var scene = document.getElementById('sceneFour');
+var parallaxInstance = new Parallax(scene);
+
+var element = document.getElementById('sceneFour');
+element.style.removeProperty("transform");
+element.style.removeProperty("position");
+element.style.removeProperty("transform-style");
+
+var element = document.getElementById('bubblejsFour');
+element.style.removeProperty("position");
+element.style.removeProperty("left");
+element.style.removeProperty("top");
---
+++ my js/menubar.js
@@ -0,0 +1,11 @@
+function openMenu(){
+    document.getElementById("menuBar").style.right = "0px"
+}
+
+function closeMenu(){
+    document.getElementById("menuBar").style.right = "-1200px"
+}
+
+
+
+
